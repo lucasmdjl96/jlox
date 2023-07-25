b8b10a6cf76e2008364fd066d00b7dc6f74edb4f
lucasmdjl96 <lucasmdjl96@gmail.com>
2023-07-25 21:10:28 +0200

    Scanner + Parser

---
+++ src/main/java/com/ldldevelopers/lox/Parser.java
@@ -0,0 +1,165 @@
+package com.ldldevelopers.lox;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+class Parser {
+    private static class ParseError extends RuntimeException {}
+
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        return leftAssociativeBinaryExpr(
+                this::comparison,
+                TokenType.BANG_EQUAL,
+                TokenType.EQUAL_EQUAL
+        );
+    }
+
+    private Expr comparison() {
+        return leftAssociativeBinaryExpr(
+                this::term,
+                TokenType.GREATER,
+                TokenType.GREATER_EQUAL,
+                TokenType.LESS,
+                TokenType.LESS_EQUAL
+        );
+    }
+
+    private Expr term() {
+        return leftAssociativeBinaryExpr(
+                this::factor,
+                TokenType.PLUS,
+                TokenType.MINUS
+        );
+    }
+
+    private Expr factor() {
+        return leftAssociativeBinaryExpr(
+                this::unary,
+                TokenType.STAR,
+                TokenType.SLASH
+        );
+    }
+
+    private Expr leftAssociativeBinaryExpr(Supplier<Expr> higher, TokenType... types) {
+        Expr expr = higher.get();
+
+        while (match(types)) {
+            Token operator = previous();
+            Expr right = higher.get();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(TokenType.BANG, TokenType.MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(TokenType.FALSE)) return new Expr.Literal(false);
+        if (match(TokenType.TRUE)) return new Expr.Literal(true);
+        if (match(TokenType.NIL)) return new Expr.Literal(null);
+
+        if (match(TokenType.NUMBER, TokenType.STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(TokenType.LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expect expression.");
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == TokenType.EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == TokenType.SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+
+            advance();
+        }
+    }
+}
---
+++ src/main/java/com/ldldevelopers/lox/TokenType.java
@@ -0,0 +1,17 @@
+package com.ldldevelopers.lox;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, OR, CLASS, IF, ELSE, TRUE, FALSE, NIL, FUN, VAR, FOR, WHILE, RETURN, SUPER, THIS, PRINT,
+
+    EOF
+}
