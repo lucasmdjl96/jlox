e4faf002e6ee10157ebcf39a0b192598f3dfbc06
Lucas M de Jong Larrarte <lucasmdjl96@gmail.com>
2023-08-11 16:25:19 +0200

    Functions

--- src/main/java/com/ldldevelopers/lox/Interpreter.java
+++ src/main/java/com/ldldevelopers/lox/Interpreter.java
@@ -1,9 +1,30 @@
 package com.ldldevelopers.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+    
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 0;
+            }
+            
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+            
+            @Override
+            public String toString() {
+                return "<native fn>";
+            }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
@@ -71,7 +92,29 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         // Unreachable
         return null;
     }
-
+    
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+        
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+        
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+        }
+        
+        LoxCallable function = (LoxCallable)callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(
+                expr.paren, "Expected " + function.arity() + " arguments but got " + arguments.size() + "."
+            );
+        }
+        return function.call(this, arguments);
+    }
+    
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr);
@@ -163,7 +206,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         evaluate(stmt.expression);
         return null;
     }
-
+    
+    @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        LoxFunction function = new LoxFunction(stmt, environment);
+        environment.define(stmt.name.lexeme, function);
+        return null;
+    }
+    
     @Override
     public Void visitIfStmt(Stmt.If stmt) {
         if (isTruthy(evaluate(stmt.condition))) {
@@ -180,21 +230,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         System.out.println(stringify(value));
         return null;
     }
-
-    private String stringify(Object object) {
-        if (object == null) return "nil";
-
-        if (object instanceof Double) {
-            String text = object.toString();
-            if (text.endsWith(".0")) {
-                text = text.substring(0, text.length() - 2);
-            }
-            return text;
-        }
-
-        return object.toString();
+    
+    @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        Object value = null;
+        if (stmt.value != null) value = evaluate(stmt.value);
+        throw new Return(value);
     }
-
+    
     @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         Object value = null;
@@ -212,5 +255,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
             execute(stmt.body);
         }
         return null;
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 }
--- src/main/java/com/ldldevelopers/lox/Stmt.java
+++ src/main/java/com/ldldevelopers/lox/Stmt.java
@@ -3,96 +3,126 @@ package com.ldldevelopers.lox;
 import java.util.List;
 
 abstract class Stmt {
-    abstract <R> R accept(Visitor<R> visitor);
     interface Visitor<R> {
         R visitBlockStmt(Block stmt);
         R visitExpressionStmt(Expression stmt);
+        R visitFunctionStmt(Function stmt);
         R visitIfStmt(If stmt);
         R visitPrintStmt(Print stmt);
+        R visitReturnStmt(Return stmt);
         R visitVarStmt(Var stmt);
         R visitWhileStmt(While stmt);
     }
-
     static class Block extends Stmt {
-        final List<Stmt> statements;
-
         Block(List<Stmt> statements) {
             this.statements = statements;
         }
+
+        final List<Stmt> statements;
 
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitBlockStmt(this);
         }
     }
-
     static class Expression extends Stmt {
-        final Expr expression;
-
         Expression(Expr expression) {
             this.expression = expression;
         }
+
+        final Expr expression;
 
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitExpressionStmt(this);
         }
     }
+    static class Function extends Stmt {
+        Function(Token name, List<Token> params, List<Stmt> body) {
+            this.name = name;
+            this.params = params;
+            this.body = body;
+        }
 
+        final Token name;
+        final List<Token> params;
+        final List<Stmt> body;
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitFunctionStmt(this);
+        }
+    }
     static class If extends Stmt {
-        final Expr condition;
-        final Stmt thenBranch;
-        final Stmt elseBranch;
         If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
             this.condition = condition;
             this.thenBranch = thenBranch;
             this.elseBranch = elseBranch;
         }
 
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch;
+
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitIfStmt(this);
         }
     }
-
     static class Print extends Stmt {
-        final Expr expression;
-
         Print(Expr expression) {
             this.expression = expression;
         }
+
+        final Expr expression;
 
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitPrintStmt(this);
         }
     }
+    static class Return extends Stmt {
+        Return(Token keyword, Expr value) {
+            this.keyword = keyword;
+            this.value = value;
+        }
 
+        final Token keyword;
+        final Expr value;
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitReturnStmt(this);
+        }
+    }
     static class Var extends Stmt {
-        final Token name;
-        final Expr initializer;
         Var(Token name, Expr initializer) {
             this.name = name;
             this.initializer = initializer;
         }
+
+        final Token name;
+        final Expr initializer;
 
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitVarStmt(this);
         }
     }
-
     static class While extends Stmt {
-        final Expr condition;
-        final Stmt body;
         While(Expr condition, Stmt body) {
             this.condition = condition;
             this.body = body;
         }
-        
+
+        final Expr condition;
+        final Stmt body;
+
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitWhileStmt(this);
         }
     }
+
+    abstract <R> R accept(Visitor<R> visitor);
 }
